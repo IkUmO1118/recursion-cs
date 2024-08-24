b31134de68da9bca9176bee833f648b6aa19e0cc
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-08-24 13:31:32 +0900

    Added system for displaying messages to other users.

--- backend-project-2/online-chat-messenger/server.py
+++ backend-project-2/online-chat-messenger/server.py
@@ -1,3 +1,35 @@
+# import socket
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# server_address = '0.0.0.0'
+# server_port = 9005
+
+# sock.bind((server_address, server_port))
+
+# print('starting up on port {}'.format(server_port))
+
+# clients = {}
+
+# while True:
+#   data, address = sock.recvfrom(4096)
+  
+#   print("received {} bytes from {}".format(len(data), address))
+
+#   if data:
+#     decoded_data = data.decode("utf-8")
+
+#     username_len, username, message = decoded_data.split(':', 2)
+#     print(username)
+#     print(message)
+
+#     if address not in clients:
+#       clients[address] = username
+
+#     response = f'{username}: {message}'
+
+#     for client_address in clients.keys():
+#       sent = sock.sendto(response.encode('utf-8'), client_address)
+
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -6,10 +38,25 @@ server_port = 9005
 
 sock.bind((server_address, server_port))
 
-print('starting up on port {}'.format(server_port))
+print(f'Starting up on port {server_port}')
+
+clients = {}
 
 while True:
-  data, address = sock.recvfrom(4096)
-  data = data.decode('utf-8')
+    data, address = sock.recvfrom(4096)
+    
+    print(f"Received {len(data)} bytes from {address}")
 
-  print(data)
+    if data:
+        decoded_data = data.decode("utf-8")
+        username_len, username, message = decoded_data.split(':', 2)
+        print(f"Username: {username}")
+        print(f"Message: {message}")
+
+        if address not in clients:
+            clients[address] = username
+
+        response = f'{username}: {message}'
+
+        for client_address in clients.keys():
+            sock.sendto(response.encode('utf-8'), client_address)
