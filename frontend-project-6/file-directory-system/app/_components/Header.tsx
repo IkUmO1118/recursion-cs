f1877066b5ea48634f8ce909d1d8dc9f5018c858
IkUmO1118 <132073192+IkUmO1118@users.noreply.github.com>
2024-10-01 11:39:00 +0900

    Delete frontend-project-6/file-directory-system directory

--- frontend-project-6/file-directory-system/app/_components/Header.tsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <div className='bg-white h-10 rounded-t-md flex px-4'>
-      <p className='font-bold text-xl self-center'>Visual File System</p>
-    </div>
-  );
-}
-
-export default Header;
