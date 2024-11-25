a1f849180bea872c1f1a9c21b788845059d44cfd
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-11-25 23:52:29 +0900

    Settings

---
+++ backend-project-4/restaurant-chain-mockup-extension/view/generate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href='../css/style.css' />
+  <title>Generate Restaurant Chains</title>
+</head>
+
+<body class="bg-white">
+  <div class="vh-100 grid grid-rows-header">
+    <section class="flex justify-between">
+      <h1 class="self-center">Randome Restaurant Chain Generator</h1>
+      <p class="text-base self-center">Recursion</p>
+    </section>
+    <section class="mx-18 grid grid-columns-sidebar">
+      <h2 class="bg-blue ">使用方法</h2>
+      <h2 class="bg-green">FORM</h2>
+    </section>
+  </div>
+</body>
+
+</html>
