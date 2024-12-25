0c38b61be5c2eafa86f0b04849fb5ac519672855
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-12-25 21:00:18 +0900

    Change from tailwindcss to pure css

--- backend-project-4/markdown-to-html/src/converter.php
+++ backend-project-4/markdown-to-html/src/converter.php
@@ -1,5 +1,18 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$Parsedown = new Parsedown();
-echo $Parsedown->text('Hello, **world**!');
+$req_json = file_get_contents('php://input');
+$req = json_decode($req_json, true);
+
+$parsedown = new Parsedown();
+$convertedContent = $parsedown->text($req['content']);
+$format = $req['format'];
+
+if ($format === 'download') {
+  header('Content-Type: text/html');
+  header('Content-Disposition: attachment; filename="converted.txt"');
+  echo $convertedContent;
+} else {
+  header('Content-Type: text/html');
+  echo $convertedContent;
+}
--- backend-project-4/markdown-to-html/src/editor.js
+++ backend-project-4/markdown-to-html/src/editor.js
@@ -11,7 +11,74 @@ require(['vs/editor/editor.main'], function () {
   });
 
   editor.getModel().onDidChangeContent((e) => {
-    console.log(editor.getValue());
-    console.log(e);
+    const content = editor.getValue();
+    console.log(!content);
+
+    if (content) {
+      convert(content, getCurrentView());
+    }
+  });
+
+  // init event listner
+  const downloadForm = document.getElementById('md-form');
+  downloadForm.addEventListener('submit', (_) => {
+    convert(editor.getValue(), 'download');
+  });
+
+  document.querySelectorAll('input[name="view"]').forEach((el) => {
+    el.addEventListener('change', (e) => {
+      const content = editor.getValue();
+      if (content) convert(content, e.target.value);
+    });
   });
 });
+
+async function post(content, format) {
+  try {
+    const res = await fetch('./src/converter.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content, format }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    if (format === 'download') {
+      const blob = await res.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'converted.html';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } else {
+      const htmlContent = await res.text();
+      return htmlContent;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function convert(content, format) {
+  if (format === 'download') {
+    await post(content, format);
+  } else {
+    const convertedContent = await post(content, format);
+    const previewContentEl = document.getElementById('content');
+
+    if (format === 'preview') {
+      previewContentEl.innerHTML = convertedContent;
+    } else {
+      previewContentEl.innerText = convertedContent;
+    }
+  }
+}
+
+function getCurrentView() {
+  return document.querySelector('input[name="view"]:checked').value;
+}
