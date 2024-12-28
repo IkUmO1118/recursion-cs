1cc02de9112f450723ed3e39979b2f14c61f6f99
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-12-28 16:35:34 +0900

    Added PlantUML Server project

---
+++ backend-project-4/plant-uml-server/public/index.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . './../vendor/autoload.php';
+use function Jawira\PlantUml\encodep;
+
+$encode = encodep('
+    Alice -> Bob: hello
+');
+
+
+$svg = file_get_contents("https://www.plantuml.com/plantuml/svg/{$encode}");
+echo $svg;
---
+++ backend-project-4/plant-uml-server/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5cff1728a1c664492e281540715b38356d975fec',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5cff1728a1c664492e281540715b38356d975fec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jawira/plantuml-encoding' => array(
+            'pretty_version' => 'v1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => 'fe8bce2d7ff5bb5cccf374349999cef7d6246a32',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jawira/plantuml-encoding',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
