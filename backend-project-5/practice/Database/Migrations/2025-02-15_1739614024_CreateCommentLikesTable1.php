6b66debd7c168915d5c500c29e429befb6f7e293
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-15 19:10:31 +0900

    Implemented commentLike table

---
+++ backend-project-5/practice/Database/Migrations/2025-02-15_1739614024_CreateCommentLikesTable1.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateCommentLikesTable1 implements SchemaMigration
+{
+  public function up(): array
+  {
+    // マイグレーションロジックをここに追加してください
+    return [
+      "CREATE TABLE IF NOT EXISTS commentLikes (
+        user_id BIGINT,
+        comment_id INT,
+        PRIMARY KEY (user_id, comment_id),
+        CONSTRAINT fk_commentLikes_users FOREIGN KEY (user_id) REFERENCES users(id),
+        CONSTRAINT fk_commentLikes_comments FOREIGN KEY (comment_id) REFERENCES comments(id)
+      )"
+    ];
+  }
+  public function down(): array
+  {
+    // ロールバックロジックを追加してください
+    return [];
+  }
+}
