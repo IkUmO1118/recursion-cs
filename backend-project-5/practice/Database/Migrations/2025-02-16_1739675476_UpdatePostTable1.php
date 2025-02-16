fc4ed4f058c9e955651fada1df30fc3f59f532d0
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-16 12:15:50 +0900

    Implemented UpdatePostTable1

---
+++ backend-project-5/practice/Database/Migrations/2025-02-16_1739675476_UpdatePostTable1.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class UpdatePostTable1 implements SchemaMigration
+{
+  public function up(): array
+  {
+    // マイグレーションロジックをここに追加してください
+    return [
+      "ALTER TABLE posts
+        ADD COLUMN category_id INT,
+        ADD CONSTRAINT fk_posts_category FOREIGN KEY (category_id) REFERENCES categories(id)
+      ",
+    ];
+  }
+  public function down(): array
+  {
+    // ロールバックロジックを追加してください
+    return [
+      "ALTER TABLE posts
+        DROP FOREIGN KEY fk_posts_category,
+        DROP COLUMN category_id
+      "
+    ];
+  }
+}
