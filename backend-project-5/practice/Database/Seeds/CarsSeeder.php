e96e6960eee3d42be7b1884effed5eaa1498371d
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-24 00:05:03 +0900

    Implemented テーブルの初期データ投入(4):課題

--- backend-project-5/practice/Database/Seeds/CarsSeeder.php
+++ backend-project-5/practice/Database/Seeds/CarsSeeder.php
@@ -1,16 +1,48 @@
 <?php
-namespace Database\Seeds; 
-use Database\AbstractSeeder; 
-class CarsSeeder extends AbstractSeeder { 
+
+namespace Database\Seeds;
+
+use Faker\Factory;
+use Database\AbstractSeeder;
+
+class CarsSeeder extends AbstractSeeder
+{
   // TODO: tableName文字列を割り当ててください。 
-  protected ?string $tableName = null; 
+  protected ?string $tableName = "cars";
 
   // TODO: tableColumns配列を割り当ててください。 
-  protected array $tableColumns = []; 
+  protected array $tableColumns = [
+    ['data_type' => 'string', 'column_name' => 'make'],
+    ['data_type' => 'string', 'column_name' => 'model'],
+    ['data_type' => 'int', 'column_name' => 'year'],
+    ['data_type' => 'string', 'column_name' => 'color'],
+    ['data_type' => 'float', 'column_name' => 'price'],
+    ['data_type' => 'float', 'column_name' => 'mileage'],
+    ['data_type' => 'string', 'column_name' => 'transmission'],
+    ['data_type' => 'string', 'column_name' => 'engine'],
+    ['data_type' => 'string', 'column_name' => 'status'],
+  ];
 
-  public function createRowData(): array 
-  { 
+  public function createRowData(): array
+  {
     // TODO: createRowData()メソッドを実装してください。 
-    return []; 
+    $faker = Factory::create();
+    $cars = [];
+
+    for ($i = 0; $i < 1000; $i++) {
+      $cars[] = [
+        $faker->company, // make
+        $faker->word,    // model
+        (int)$faker->year,    // year
+        $faker->safeColorName, // color
+        $faker->randomFloat(2, 10000, 50000), // price
+        $faker->randomFloat(2, 0, 200000), // mileage
+        $faker->randomElement(['Automatic', 'Manual']), // transmission
+        $faker->word,    // engine
+        $faker->randomElement(['New', 'Used']), // status
+      ];
+    }
+
+    return $cars;
   }
-} 
+}
--- backend-project-5/practice/Database/Seeds/PartsSeeder.php
+++ backend-project-5/practice/Database/Seeds/PartsSeeder.php
@@ -1,16 +1,41 @@
 <?php
-namespace Database\Seeds; 
-use Database\AbstractSeeder; 
-class PartsSeeder extends AbstractSeeder { 
+
+namespace Database\Seeds;
+
+use Faker\Factory;
+use Database\AbstractSeeder;
+
+class PartsSeeder extends AbstractSeeder
+{
   // TODO: tableName文字列を割り当ててください。 
-  protected ?string $tableName = null; 
+  protected ?string $tableName = "parts";
 
   // TODO: tableColumns配列を割り当ててください。 
-  protected array $tableColumns = []; 
+  protected array $tableColumns = [
+    ['data_type' => 'int', 'column_name' => 'carID'],
+    ['data_type' => 'string', 'column_name' => 'name'],
+    ['data_type' => 'string', 'column_name' => 'description'],
+    ['data_type' => 'float', 'column_name' => 'price'],
+    ['data_type' => 'int', 'column_name' => 'quantityInStock'],
+  ];
 
-  public function createRowData(): array 
-  { 
+  public function createRowData(): array
+  {
     // TODO: createRowData()メソッドを実装してください。 
-    return []; 
+    $faker = Factory::create();
+    $parts = [];
+    $carIDs = range(1, 1000); // Assuming car IDs are from 1 to 1000
+
+    for ($i = 0; $i < 100000; $i++) {
+      $parts[] = [
+        $faker->randomElement($carIDs), // carID
+        $faker->word, // name
+        $faker->sentence, // description
+        $faker->randomFloat(2, 10, 500), // price
+        $faker->numberBetween(1, 100), // quantityInStock
+      ];
+    }
+
+    return $parts;
   }
-} 
+}
